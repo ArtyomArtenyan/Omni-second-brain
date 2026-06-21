4a1778fcb6ddf2b7a982f327e67e613e8044b17c
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2026-06-21 21:13:06 +0400

    feat: setup Better Auth

---
+++ lib/auth/index.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaClient } from '@/lib/generated/prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+
+const adapter = new PrismaNeon({
+	connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	secret: process.env.BETTER_AUTH_SECRET,
+	baseURL: process.env.BETTER_AUTH_URL,
+});
